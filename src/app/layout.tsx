ed2ec44a271aacd2ddd2a2575a3469d619be00dc
soezyxstt <16922204@mahasiswa.itb.ac.id>
2024-07-10 18:42:16 +0700

    add scoping

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import './globals.css';
+import { cn } from '@/lib/utils';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: 'HMM ITB',
+    template: '%s | HMM ITB',
+    absolute: 'HMM ITB',
+  },
+  description: 'PWA for HMM ITB',
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <html lang='en'>
+      <body className={cn(poppins.className, 'md:flex')}>
+        {children}
+      </body>
+    </html>
+  );
+}
